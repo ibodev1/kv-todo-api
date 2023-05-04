cc7ef38075e3318f66836360fba647ac36e2c183
İbrahim Ödev <74017534+ibodev1@users.noreply.github.com>
2023-05-04 12:10:11 +0300

    update: add tests

--- main_test.ts
+++ main_test.ts
@@ -1,9 +1,27 @@
-import { assertEquals } from "https://deno.land/std@0.184.0/testing/asserts.ts"
+import { assertEquals } from "$std/testing/asserts.ts"
 import { Hono } from "hono"
+import { getLastTodos } from './utils/lib.ts'
+import { getAllSubjects } from "./utils/db/subject.ts"
 
 Deno.test('Server Test', async () => {
   const app = new Hono()
   app.get('/', (c) => c.text('Please test me'))
   const res = await app.request('http://localhost/')
   assertEquals(res.status, 200)
+})
+
+Deno.test('Todo Test', async () => {
+  const todos = await getLastTodos()
+  assertEquals(todos.length > 0, true)
+
+  const firstTodo = todos[0];
+  assertEquals(typeof firstTodo.isDone, "boolean")
+})
+
+Deno.test('Subject Test', async () => {
+  const subjects = await getAllSubjects()
+  assertEquals(subjects.length > 0, true)
+  
+  const firstSubjet = subjects[0];
+  assertEquals(typeof firstSubjet !== "undefined", true)
 })
